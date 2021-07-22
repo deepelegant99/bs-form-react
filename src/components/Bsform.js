4e295d61b3ad320fbc3b031a5506514605063781
Phillip Wong <deepelegant99@gmail.com>
2021-07-21 23:25:56 -0700

    Add functionality such that when one or more field is empty, error message is outputed

--- src/components/Bsform.js
+++ src/components/Bsform.js
@@ -1,50 +1,68 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 
-const Bsform = () =>{
+const Bsform = () => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [err, setErr] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const [name, setName]=useState('');
-    const [address, setAddress]=useState('');
-
-    const onclickHandlier = (e)=>{
-        e.preventDefault()
-        if (name==='' || address==''){
-            console.log("please fill out the fields")
-        }
+  useEffect(() => {
+    if (err === true) {
+      setErrorMessage("Please enter required fields");
     }
+  }, [err]);
 
-    const nameHandlier = (e)=>{
-        setName(e.target.value)
+  const onclickHandlier = (e) => {
+    e.preventDefault();
+    if (name === "" || address == "") {
+      setErr(true);
     }
+  };
 
-    const addressHandlier = (e)=>{
-        setAddress(e.target.value)
-    }
+  const nameHandlier = (e) => {
+    setName(e.target.value);
+  };
 
-    return (
-    <section class="container">
+  const addressHandlier = (e) => {
+    setAddress(e.target.value);
+  };
+
+  return (
+    <section className="container">
       <form id="my-form">
         <h1>Sign up for our newsletter</h1>
-        <div class="msg"></div>
+        {/* <div className="msg"></div> */}
+        <div className={`${err ? "error" : ""}`}>{errorMessage}</div>
         <div>
           <label for="name">Name:</label>
           <input onChange={nameHandlier} type="text" id="name" value={name} />
         </div>
         <div>
           <label for="email">Email:</label>
-          <input onChange={addressHandlier} type="text" id="email" value={address} />
+          <input
+            onChange={addressHandlier}
+            type="text"
+            id="email"
+            value={address}
+          />
         </div>
-        <input onClick={onclickHandlier} class="btn" type="submit" value="Submit" />
+        <input
+          onClick={onclickHandlier}
+          className="btn"
+          type="submit"
+          value="Submit"
+        />
       </form>
-{/* 
+      {/* 
       <ul id="users"></ul>
 
-      <ul class="items">
-          <li class="item">Item 1</li>
-          <li class="item">Item 2</li>
-          <li class="item">Item 3</li>
+      <ul className="items">
+          <li className="item">Item 1</li>
+          <li className="item">Item 2</li>
+          <li className="item">Item 3</li>
         </ul>  */}
     </section>
-    );
-}
+  );
+};
 
 export default Bsform;
